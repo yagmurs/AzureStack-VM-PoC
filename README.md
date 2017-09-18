015c1c26ca4f42811043532ee62b8fba28de3c6e
Yagmur Sahin <yagmurs@hotmail.com>
2017-09-18 21:51:02 +0300

    v0.0.3

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Creates a new VM to install AzureStack Develeopment kit (ASDK) to run PoC
+
 <a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fyagmurs%2FAzureStack-VM-PoC%2Fmaster%2Fazuredeploy.json" target="_blank">
     <img src="https://azuredeploy.net/deploybutton.png"/>
 </a>
@@ -6,3 +8,11 @@
     <img src="https://raw.githubusercontent.com/shenglol/arm-visualizer/master/src/visualizebutton.png"/>
 </a>
 
+This template creates a new VM to easily evaluate AzureStack PoC on an Azure VM, and installs most of the prerequisites such as Failover cluster, hyper-v and management modules.
+
+Tips
+
+If running under PowerShell you may update the azuredeploy.parameters file.
+Customize parameters in azuredeploy.parameters as you see appropriate, at the very least the adminPassword.
+
+Feel free to post questions and enjoy!
--- scripts/post-config.ps1
+++ scripts/post-config.ps1
@@ -7,6 +7,8 @@ New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\A
 New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\ -Name AllowFreshCredentialsWhenNTLMOnly -Value 1 -Force
 New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly\ -Name 1 -Value  'WSMAN/*' -Force
 Get-Disk | Where-Object {$_.partitionstyle -eq 'raw' -and $_.size -eq "64GB"} | Initialize-Disk -PartitionStyle GPT -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Downloads" -Confirm:$false
+$size = Get-Disk -Number 0 | Get-Partition | Get-PartitionSupportedSize
+Resize-Partition -DiskNumber 0 -PartitionNumber 1 -Size $size.SizeMax
 Add-WindowsFeature RSAT-AD-PowerShell, RSAT-ADDS -IncludeAllSubFeature
 Install-PackageProvider nuget -Force
 Rename-LocalUser -Name $username -NewName Administrator

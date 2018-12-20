9d9518b79e1b1868831777687eba5668223bf740
Yagmur Sahin <yagmurs@microsoft.com>
2018-12-20 16:48:32 +0300

    new powershell deployment script

--- scripts/cleanup and deploy.ps1
+++ scripts/cleanup and deploy.ps1
@@ -1,44 +1,38 @@
-﻿$resourceGroupName = "azs3"
-if (-not (Get-AzureRmSubscription))
-{
-    Login-AzureRmAccount
-}
-Get-AzureRmVM -ResourceGroupName $resourceGroupName | Remove-AzureRmVM -Force
-Get-AzureRmNetworkInterface -ResourceGroupName $resourceGroupName | Remove-AzureRmNetworkInterface -Force
-Get-AzureRmNetworkSecurityGroup -ResourceGroupName $resourceGroupName | Remove-AzureRmNetworkSecurityGroup -Force
-Get-AzureRmPublicIpAddress -ResourceGroupName $resourceGroupName | Remove-AzureRmPublicIpAddress -Force
-$disks = Get-AzureRmDisk
-if ($resourceGroupName -like "*test*")
-{
-    $disks | Remove-AzureRmDisk -Force
-}
-else
-{
-    $disks | ? name -like *_OSDisk_* | Remove-AzureRmDisk -Force
-    $disks | ? name -like *-disk* | Remove-AzureRmDisk -Force
-}
-Get-AzureRmVirtualNetwork -ResourceGroupName $resourceGroupName | Remove-AzureRmVirtualNetwork -Force
+﻿$tenantID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+$subscriptionID = "yyyyyyyy-yyyy-yyyy-yyyy-yyyyyyyyyyyy"
 
-Remove-AzureRmResourceGroup -ResourceGroupName $resourceGroupName -Force
+Connect-AzureRmAccount -TenantId $tenantID -Subscription $subscriptionID
 
-Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName `
-  -TemplateUri https://raw.githubusercontent.com/yagmurs/yagmurs_AzureVMExtension/master/azuredeploy.json `
-  -TemplateParameterUri https://raw.githubusercontent.com/yagmurs/yagmurs_AzureVMExtension/master/azuredeploy.parameters.jsonRemove-AzureRmResourceGroup -Force
+$SecureAdminPassword = Read-Host -AsSecureString -Prompt "Provide local Administrator password for Azure Stack host VM" | ConvertTo-SecureString -AsPlainText -Force
+#$SecureAdminPassword = "SuperSecurePassword123!!" | ConvertTo-SecureString -AsPlainText -Force
 
-New-AzureRmResourceGroup -Name $resourceGroupName -Location 'west europe'
+[int]$instanceNumber = 1
+[bool]$autoDownloadASDK = $false
+[string]$resourceGroupNamePrefix = "yagmursasdk"
+[string]$publicDnsNamePrefix = "yagmursasdkinstance"
+[string]$location = 'East US2'
+[ValidateSet("development", "master")][string]$gitBranch = "development"
+
+
+
+[string]$resourceGroupName = "$resourceGroupNamePrefix-$instanceNumber"
+[string]$publicDnsName = "$publicDnsNamePrefix$instanceNumber"
+$templateParameterObject = @{}
+$templateParameterObject.Add("adminPassword", $SecureAdminPassword)
+$templateParameterObject.Add("publicDnsName",$publicDnsName.ToLower())
+$templateParameterObject.Add("autoDownloadASDK", $autoDownloadASDK)
+
+# create resource group
+New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+
+# deploy ARM template from github using locally provided ARM template parameters
 New-AzureRmResourceGroupDeployment -Name "$resourceGroupName-PoC-Deployment" -ResourceGroupName $resourceGroupName `
-  -TemplateUri https://raw.githubusercontent.com/yagmurs/yagmurs_AzureVMExtension/master/azuredeploy.json `
-  -TemplateParameterUri https://raw.githubusercontent.com/yagmurs/yagmurs_AzureVMExtension/master/azuredeploy.parameters.json `
-  -Mode Incremental
-
-New-AzureRmResourceGroupDeployment -Name "$resourceGroupName-PoC-Deployment" -ResourceGroupName $resourceGroupName `
-  -TemplateUri https://raw.githubusercontent.com/yagmurs/yagmurs_AzureVMExtension/master/azuredeploy.json `
-  -Mode Incremental
+    -TemplateUri "https://raw.githubusercontent.com/yagmurs/AzureStack-VM-PoC/$gitBranch/azuredeploy.json" `
+    -TemplateParameterObject $templateParameterObject `
+    -Mode Incremental `
+    -AsJob
 
 
-$swName = "NATSwitch"
-New-VMSwitch -Name $swName -SwitchType Internal -Verbose
-$NIC=Get-NetAdapter "vEthernet `($swName`)"
-New-NetIPAddress -IPAddress 172.16.0.1 -PrefixLength 24 -InterfaceIndex $NIC.ifIndex
-New-NetNat -Name $swName -InternalIPInterfaceAddressPrefix "172.16.0.0/24" –Verbose
-Get-VM -Name AzS-BGPNAT01 | Get-VMNetworkAdapter -Name NAT | Connect-VMNetworkAdapter -SwitchName $swName
+Pause
+#clean up 
+Get-AzureRmResourceGroup -Name $resourceGroupName | Remove-AzureRmResourceGroup -AsJob #-Force

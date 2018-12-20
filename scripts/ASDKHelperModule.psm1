9eccb774d6c40bbbdbf141b996e1429e26fcfc96
Yagmur Sahin <yagmurs@microsoft.com>
2018-12-21 01:35:49 +0300

    new function Start-SleepWithProgress

--- scripts/ASDKHelperModule.psm1
+++ scripts/ASDKHelperModule.psm1
@@ -306,3 +306,24 @@ function Disable-ICS ($PublicAdapterName, $PrivateAdapterName)
     $publicAdapter.DisableSharing()
     $privateAdapter.DisableSharing()
 }
+
+function Start-SleepWithProgress($seconds)
+{
+
+    $doneDT = (Get-Date).AddSeconds($seconds)
+
+    while($doneDT -gt (Get-Date)) {
+
+        $secondsLeft = $doneDT.Subtract((Get-Date)).TotalSeconds
+
+        $percent = ($seconds - $secondsLeft) / $seconds * 100
+
+        Write-Progress -Activity "Sleeping" -Status "Sleeping..." -SecondsRemaining $secondsLeft -PercentComplete $percent
+
+        [System.Threading.Thread]::Sleep(500)
+
+    }
+
+    Write-Progress -Activity "Sleeping" -Status "Sleeping..." -SecondsRemaining 0 -Completed
+
+}

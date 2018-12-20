fa5db685a8b12a09d8e9f59a5b3ef8d471c578eb
Yagmur Sahin <yagmurs@microsoft.com>
2018-12-21 01:57:25 +0300

    Copy-ASDKContent

--- scripts/ASDKHelperModule.psm1
+++ scripts/ASDKHelperModule.psm1
@@ -331,7 +331,7 @@ function Start-SleepWithProgress($seconds)
 function Copy-ASDKContent 
 {
     param (
-        $vhdxFullPath,
+        $vhdxFullPath
     )
     $foldersToCopy = @('CloudDeployment', 'fwupdate', 'tools')
 

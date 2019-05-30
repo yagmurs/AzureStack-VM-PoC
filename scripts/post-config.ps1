fef219427ec93774ccf033800299884bd2417f27
Yagmur Sahin <yagmurs@microsoft.com>
2019-05-31 02:46:59 +0300

    update

--- scripts/post-config.ps1
+++ scripts/post-config.ps1
@@ -344,14 +344,17 @@ if ($AutoInstallASDK)
 
 $AutoInstallASDKsb = @"
 
+    if ((Test-Path -Path "D:\Azure Stack Development Kit\cloudbuilder.vhdx") -and (Test-Path -Path "c:\CloudDeployment"))
+    {
+        Get-Scheduledtask -TaskName "$taskName3" | Disable-ScheduledTask 
+    }
     `$lPass = "$LocalAdminPass" | ConvertTo-SecureString -AsPlainText -Force
     `$aadPass = "$AzureADGlobalAdminPass" | ConvertTo-SecureString -AsPlainText -Force
     `$InfraAzureDirectoryTenantAdminCredential = New-Object System.Management.Automation.PSCredential ("$AzureADGlobalAdmin", `$aadPass)
     $defaultLocalPath\Install-ASDK.ps1 -DownloadASDK -DeploymentType $deploymentType -LocalAdminPass `$lPass -AADTenant $AzureADTenant -InfraAzureDirectoryTenantAdminCredential `$InfraAzureDirectoryTenantAdminCredential -Version $version
-    Get-Scheduledtask -TaskName "$taskName3" | Disable-ScheduledTask    
+   
 "@
 $AutoInstallASDKScriptBlock = [scriptblock]::Create($AutoInstallASDKsb)
-
 
     if (Get-ScheduledJob -name $taskName3 -ErrorAction SilentlyContinue)
     {

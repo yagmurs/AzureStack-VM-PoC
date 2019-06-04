052e6e88e712163aa8610526e29b94192af6a682
Yagmur Sahin <yagmurs@microsoft.com>
2019-06-04 23:20:00 +0300

    update

--- scripts/post-config.ps1
+++ scripts/post-config.ps1
@@ -129,12 +129,12 @@ if ($ASDKConfiguratorObject)
         #download iso files
         if ($ASDKConfiguratorParams.IsoPath -like "*WS2019EVALISO.iso")
         {
-            DownloadWithRetry -Uri "https://software-download.microsoft.com/download/pr/17763.253.190108-0006.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso" -DownloadLocation $ASDKConfiguratorParams.IsoPath
+            DownloadWithRetry -Uri https://software-download.microsoft.com/download/pr/17763.253.190108-0006.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso -DownloadLocation $ASDKConfiguratorParams.IsoPath
         }
         if ($ASDKConfiguratorParams.IsoPath -like "*WS2016EVALISO.iso")
         {
             $ProgressPreference = "SilentlyContinue"
-            DownloadWithRetry -Uri "http://download.microsoft.com/download/1/4/9/149D5452-9B29-4274-B6B3-5361DBDA30BC/14393.0.161119-1705.RS1_REFRESH_SERVER_EVAL_X64FRE_EN-US.ISO" -DownloadLocation $ASDKConfiguratorParams.IsoPath
+            DownloadWithRetry -Uri http://download.microsoft.com/download/1/4/9/149D5452-9B29-4274-B6B3-5361DBDA30BC/14393.0.161119-1705.RS1_REFRESH_SERVER_EVAL_X64FRE_EN-US.ISO -DownloadLocation $ASDKConfiguratorParams.IsoPath
         }
         #create asdkconfigurator command to be called
         $commandToRun = "$(Join-Path -Path $AsdkConfigurator.path -ChildPath "ConfigASDK.ps1") $()"
@@ -149,11 +149,11 @@ $script = @"
 "@
         if ($ASDKConfiguratorParams.IsoPath -like "*WS2019EVALISO.iso")
         {
-            $script += "DownloadWithRetry -Uri "https://software-download.microsoft.com/download/pr/17763.253.190108-0006.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso" -DownloadLocation $($ASDKConfiguratorParams.IsoPath)"
+            $script += "DownloadWithRetry -Uri https://software-download.microsoft.com/download/pr/17763.253.190108-0006.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso -DownloadLocation $($ASDKConfiguratorParams.IsoPath)"
         }
         if ($ASDKConfiguratorParams.IsoPath -like "*WS2016EVALISO.iso")
         {
-            $script += "DownloadWithRetry -Uri "http://download.microsoft.com/download/1/4/9/149D5452-9B29-4274-B6B3-5361DBDA30BC/14393.0.161119-1705.RS1_REFRESH_SERVER_EVAL_X64FRE_EN-US.ISO" -DownloadLocation $($ASDKConfiguratorParams.IsoPath)"
+            $script += "DownloadWithRetry -Uri http://download.microsoft.com/download/1/4/9/149D5452-9B29-4274-B6B3-5361DBDA30BC/14393.0.161119-1705.RS1_REFRESH_SERVER_EVAL_X64FRE_EN-US.ISO -DownloadLocation $($ASDKConfiguratorParams.IsoPath)"
         }
 
         $script += "$(Join-Path -Path $AsdkConfigurator.path -ChildPath "ConfigASDK.ps1") $($ASDKConfiguratorParams)"

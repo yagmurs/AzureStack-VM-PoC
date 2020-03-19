ec9f008bd469dba5ea94f7c52fce9658fba661e1
Yagmur Sahin <yagmurs@microsoft.com>
2020-03-19 22:37:13 +0300

    2002

--- scripts/Install-ASDK.ps1
+++ scripts/Install-ASDK.ps1
@@ -156,7 +156,7 @@ Write-Log @writeLogParams -Message "Tweaking some files to run ASDK on Azure VM"
 Write-Log @writeLogParams -Message "Applying first workaround to tackle bare metal detection"
 workaround1
 
-if ($version -ge 1910)
+if ($version -eq 1910)
 {
     workaround4
 }

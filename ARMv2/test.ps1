62cb8fb635b2cf2ea30fe72034e623842d2e1d42
Yagmur Sahin <yagmurs@hotmail.com>
2020-11-24 11:04:05 +0300

    testing

--- ARMv2/test.ps1
+++ ARMv2/test.ps1
@@ -1,41 +1,137 @@
-Connect-AzAccount -Subscription "Internal Consumption"
+<#
+.Synopsis
+   Deploys Azure VM for Azure Stack Hub Development kit
+.DESCRIPTION
+   Deploys Azure VM for Azure Stack Hub Development kit
+.EXAMPLE
+   Deploy new Storage copy VM image and then deploys Azure Stack Hub Development kit VM to Resource Group: AzureStackVMOnAzureVM on East US 2 region, Credential will be popped up!
+Deploy-AzureStackonAzureVM
+.EXAMPLE
+   Deploy new Storage copy VM image and then deploys Azure Stack Hub Development kit VM to Resource Group: myResourceGroup on West Europe region, Credential will be popped up!
+Deploy-AzureStackonAzureVM -ResourceGroupName myResourceGroup -Region = 'West Europe'
+.EXAMPLE
+   This option deploys Azure Stack Hub Development kit from predefined Uri, this can be used if previously, Credential will be popped up!
+   ASDK image previously copied or created manually on Storage Account the storage account that this Uri belongs to has to be on the same subscription. 
+Deploy-AzureStackonAzureVM -UseExistingStorageAccount
+.EXAMPLE
+   Deploy new Storage copy VM image and then deploys Azure Stack Hub Development kit VM to Resource Group: AzureStackVMOnAzureVM Credential specified beforehand. 
+   May be used for silent deployment.
+$Credential = Get-Credentail = "Local Administrator Password"
+Deploy-AzureStackonAzureVM -ResourceGroupName myResourceGroup -Credential $Credential
+.INPUTS
+   Inputs to this cmdlet (if any)
+.OUTPUTS
+   Output from this cmdlet (if any)
+.NOTES
+   Version:        0.1
+   Author:         Yagmur Sahin
+   Twitter:        @yagmurs
+   Creation Date:  23 November 2020
+   Purpose/Change: Initial script development
+.COMPONENT
+   The component this cmdlet belongs to
+.ROLE
+   The role this cmdlet belongs to
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+#>
 
-$adminPassword = Get-Credential -Credential "Local Admin Password"
-$publicDnsName = Read-Host -Prompt "Public DNS prefix"
-$region = 'East US 2'
-$rg = 'rg-yagmurs-azsh2'
-$saName = "asdk" + (Get-Random)
-New-AzResourceGroup -Name $rg -Location $region
-$sa = New-AzStorageAccount -Location $region -ResourceGroupName $rg -SkuName Standard_LRS -Name $saName
+#Requires -Version 5
+#Requires -Module @{ ModuleName = 'Az'; RequiredVersion = '5.1.0' }
+#Requires -Module @{ ModuleName = 'Az.Accounts'; RequiredVersion = '2.2.1' }
+#Requires -Module @{ ModuleName = 'Az.Storage'; RequiredVersion = '3.0.0' }
+#Requires -Module @{ ModuleName = 'Az.Resources'; RequiredVersion = '3.0.1' }
 
-$sourceUri = "https://azshdkeus2.blob.core.windows.net/vhd/cloudbuilder.vhd"
-New-AzStorageContainer -Name vhd -Context $sa.context
-Start-AzStorageBlobCopy -AbsoluteUri $sourceUri -DestContainer "vhd" -DestContext $sa.context -DestBlob "cloudbuilder.vhd" -ConcurrentTaskCount 100
-do {
-    Start-Sleep -Seconds 60
-    $result = Get-AzStorageAccount -Name $sa.StorageAccountName -ResourceGroupName $rg | Get-AzStorageBlob -Container "vhd" | Get-AzStorageBlobCopyState
-    $remaining = [Math]::Round(($result.TotalBytes - $result.BytesCopied) / 1gb,2)
-    Write-Verbose -Message "Waiting copy to finish remaining $remaining GB" -Verbose 
-} until ($result.Status -eq "success") 
+param(
+        [Parameter(Mandatory=$false)]
+        [switch]$UseExistingStorageAccount,
+        
+        [Parameter(Mandatory=$false)]
+        [switch]$Overwrite,
 
+        [Parameter(Mandatory=$false)]
+        [string]$Region = 'East US 2',
+        
+        [Parameter(Mandatory=$false)]
+        [string]$ResourceGroupName = 'AzureStackVMOnAzureVM',
+        
+        [Parameter(Mandatory=$false)]
+        [string]$version = "2008",
+        
+        [Parameter(Mandatory=$true)]
+        [pscredential]$Credential, #Local Admin Credential for the VM
+        
+        [Parameter(Mandatory=$false)]
+        [string]$publicDnsName = "asdkonazure" + "$(get-random)",
+
+        [Parameter(Mandatory=$false)]
+        [string]$VhdUri, #this must a Azure Storage Account Uri and must be under the same subscription that the VM is getting deployed.
+
+        [Parameter(Mandatory=$false)]
+        [int]$DataDiskCount = 6
+    )
+
+Connect-AzAccount -UseDeviceAuthentication
+
+if ($UseExistingStorageAccount) 
+{
+   if ($VhdUri)
+   {
+      $saName = $VhdUri.Split(".")[0].Split("/")[-1]
+      $sa = Get-AzStorageAccount | Where-Object StorageAccountName -EQ $saName
+      if ($sa)
+      {
+         Write-Verbose -Message "Storage account: $VhdUri exist in the same subscription"
+         $uriSplit = $VhdUri.Split("/")
+         Get-AzStorageBlob -Blob $uriSplit[-1] -Container $uriSplit[-2] -Context $sa.context
+         if ($?)
+         {
+            Write-Verbose -Message "Blob file: $uriSplit[-1] exist under container: $uriSplit[-2]"
+            $osDiskVhdUri = $VhdUri
+         }
+         else 
+         {
+            Write-Error -Message "Blob file: $uriSplit[-1] does not exist under container: $uriSplit[-2]" -ErrorAction Stop
+         }   
+      }
+      else
+      {
+         Get-AzStorageAccountNameAvailability
+         Write-Error "Storage account: $VhdUri is not belongs to the subscription, please specify Storage Account from same subscription" -ErrorAction Stop    
+      }
+   }
+   else 
+   {
+      Write-Error "No VHD Uri specified" -ErrorAction Stop
+   }
+}
+else
+{
+   $saName = "asdk" + (Get-Random)
+   New-AzResourceGroup -Name $ResourceGroupName -Location $Region
+   $sa = New-AzStorageAccount -Location $Region -ResourceGroupName $ResourceGroupName -SkuName Standard_LRS -Name $saName
+
+   $sourceUri = "https://asdkstore.blob.core.windows.net/asdk/$version.vhd"
+   New-AzStorageContainer -Name "asdk" -Context $sa.context
+   Start-AzStorageBlobCopy -AbsoluteUri $sourceUri -DestContainer "asdk" -DestContext $sa.context -DestBlob "$version.vhd" -ConcurrentTaskCount 100 -Force
+   do {
+      Start-Sleep -Seconds 60
+      $result = Get-AzStorageAccount -Name $sa.StorageAccountName -ResourceGroupName $ResourceGroupName | Get-AzStorageBlob -Container "asdk" | Get-AzStorageBlobCopyState
+      $remaining = [Math]::Round(($result.TotalBytes - $result.BytesCopied) / 1gb,2)
+      Write-Verbose -Message "Waiting copy to finish remaining $remaining GB" -Verbose 
+   } until ($result.Status -eq "success") 
+
+   $osDiskVhdUri = $sa.PrimaryEndpoints.Blob + "asdk/$version.vhd"
+}
+
+Write-Verbose -Message $osDiskVhdUri
 
 $templateParameterObject = @{
-    adminPassword = $adminPassword
-    publicDnsName = $publicDnsName
-    dataDiskCount = 6
-    osDiskVhdUri = $sa.PrimaryEndpoints.Blob + "vhd/cloudbuilder.vhd"
+   adminPassword = $Credential.Password
+   publicDnsName = $publicDnsName
+   dataDiskCount = $DataDiskCount
+   osDiskVhdUri = $osDiskVhdUri
 }
-New-AzResourceGroupDeployment -ResourceGroupName $rg -Name AzureStackonAzureVM -TemplateUri "https://raw.githubusercontent.com/yagmurs/AzureStack-VM-PoC/development/ARMv2/azuredeploy.json" -TemplateParameterObject $templateParameterObject
 
-#region 1
-#region 2
-dasdoksadsd
-sad
-sadasd
-asd
-#endregion 2
-asd
-asd
-as
-
-#endregion 1
+#New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -Name AzureStackonAzureVM -TemplateUri "https://raw.githubusercontent.com/yagmurs/AzureStack-VM-PoC/development/ARMv2/azuredeploy.json" -TemplateParameterObject $templateParameterObject
+New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -Name AzureStackonAzureVM -TemplateFile .\ARMv2\azuredeploy.json -TemplateParameterObject $templateParameterObject

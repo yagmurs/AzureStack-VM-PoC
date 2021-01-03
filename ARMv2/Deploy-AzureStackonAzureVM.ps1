55bc2ff9b6e8fee1353bde01d3d8eb90634a958f
Yagmur Sahin <yagmurs@hotmail.com>
2021-01-03 22:00:07 +0300

    azcopy

--- ARMv2/Deploy-AzureStackonAzureVM.ps1
+++ ARMv2/Deploy-AzureStackonAzureVM.ps1
@@ -1,22 +1,40 @@
 <#PSScriptInfo
 
-.VERSION 0.1.1.7
+.VERSION 0.2.0.0
 
 .GUID 523642c3-73da-49a0-8ae8-08b835c426e2
 
-.AUTHOR Microsoft Corporation
+.AUTHOR Yagmur Sahin
 
 .COMPANYNAME Microsoft
 
-.EXTERNALMODULEDEPENDENCIES
+.COPYRIGHT 
 
 .TAGS Azure Stack Hub, ASDK, AzureStack, AzureStackHub, "Azure Stack", "Azure Stack Hub"
+
+.LICENSEURI 
+
+.PROJECTURI https://github.com/yagmurs/AzureStack-VM-PoC
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
 
 .RELEASENOTES
    Author:         Yagmur Sahin
    Twitter:        @yagmurs
    Creation Date:  24 November 2020
-   Purpose/Change: Updated examples and minor changes
+   Purpose/Change:
+      New AzCopy parameters added with default value.
+      Implemented AzCopy as an option copying storage blobs to extremely improve copy process. If copied within the same region (EastUS2) copy process takes at rate 5000+ Mb/s about 5 minutes, reduced from 20 minutes.
+      Cross region copy performance also increased (from East US2) to West Europe takes approximately 10 minutes at rate 2000 Mb/s
+      AzCopy will be used by default to improve the total deployment time.
+      AzCopy parameter behavior set to 'true' by default.
+
 #>
 
 <#
@@ -65,51 +83,119 @@ Deploys with default options and start Azure Stack Hub Develoepment kit installa
 the VM after VM starts. Currently there is no validation for credentials and Tenant existance
 Make sure tenant name and credentials are correct.
 #>
-[CmdletBinding(ConfirmImpact='High')]
+[CmdletBinding(
+   ConfirmImpact='High',
+   DefaultParameterSetName='VM Only'
+)]
 
-param(
-        [Parameter(Mandatory=$false)]
-        [switch]$UseExistingStorageAccount,
-        
-        [Parameter(Mandatory=$false)]
-        [switch]$Overwrite,
+param
+(
+   [Parameter(Mandatory=$false, ParameterSetName='Use Existing SA')]
+   [Parameter(Mandatory=$false, ParameterSetName='VM Only')]
+   [Parameter(Mandatory=$false, ParameterSetName='Auto Install')]
+   [switch]$UseExistingStorageAccount,
+   
+   [Parameter(Mandatory=$false, ParameterSetName='Use Existing SA')]
+   [Parameter(Mandatory=$false, ParameterSetName='VM Only')]
+   [Parameter(Mandatory=$false, ParameterSetName='Auto Install')]
+   [switch]$Overwrite,
 
-        [Parameter(Mandatory=$false)]
-        [string]$Region = 'East US 2',
-        
-        [Parameter(Mandatory=$false)]
-        [string]$ResourceGroupName = 'AzureStackVMOnAzureVM',
-        
-        [Parameter(Mandatory=$false)]
-        [string]$Version = "2008",
-        
-        [Parameter(Mandatory=$true)]
-        [pscredential]$VmCredential, #Local Administrator Credential for the VM
-        
-        [Parameter(Mandatory=$false)]
-        [string]$PublicDnsName = "asdkonazure" + "$(get-random)",
+   [Parameter(Mandatory=$false, ParameterSetName='Use Existing SA')]
+   [Parameter(Mandatory=$false, ParameterSetName='VM Only')]
+   [Parameter(Mandatory=$false, ParameterSetName='Auto Install')]
+   [string]$Region = 'East US 2',
+   
+   [Parameter(Mandatory=$false, ParameterSetName='Use Existing SA')]
+   [Parameter(Mandatory=$false, ParameterSetName='VM Only')]
+   [Parameter(Mandatory=$false, ParameterSetName='Auto Install')]
+   [string]$ResourceGroupName = 'AzureStackVMOnAzureVM',
+   
+   [Parameter(Mandatory=$false, ParameterSetName='Use Existing SA')]
+   [Parameter(Mandatory=$false, ParameterSetName='VM Only')]
+   [Parameter(Mandatory=$false, ParameterSetName='Auto Install')]
+   [string]$Version = "2008",
+   
+   [Parameter(Mandatory=$true, ParameterSetName='Use Existing SA')]
+   [Parameter(Mandatory=$true, ParameterSetName='VM Only')]
+   [Parameter(Mandatory=$true, ParameterSetName='Auto Install')]
+   [pscredential]$VmCredential, #Local Administrator Credential for the VM
+   
+   [Parameter(Mandatory=$false, ParameterSetName='Use Existing SA')]
+   [Parameter(Mandatory=$false, ParameterSetName='VM Only')]
+   [Parameter(Mandatory=$false, ParameterSetName='Auto Install')]
+   [string]$PublicDnsName = "asdkonazure" + "$(get-random)",
 
-        [Parameter(Mandatory=$false)]
-        [string]$VhdUri, #this must a Azure Storage Account Uri and must be under the same subscription that the VM is getting deployed.
+   [Parameter(Mandatory=$true, ParameterSetName='Use Existing SA')]
+   [Parameter(Mandatory=$false, ParameterSetName='VM Only')]
+   [Parameter(Mandatory=$false, ParameterSetName='Auto Install')]
+   [string]$VhdUri, #this must a Azure Storage Account Uri and must be under the same subscription that the VM is getting deployed.
 
-        [Parameter(Mandatory=$false)]
-        [int]$DataDiskCount = 6,
+   [Parameter(Mandatory=$false, ParameterSetName='Use Existing SA')]
+   [Parameter(Mandatory=$false, ParameterSetName='VM Only')]
+   [Parameter(Mandatory=$false, ParameterSetName='Auto Install')]
+   [int]$DataDiskCount = 6,
 
-        [Parameter(Mandatory=$false)]
-        [switch]$AutoInstallASDK,
+   [Parameter(Mandatory=$false, ParameterSetName='Use Existing SA')]
+   [Parameter(Mandatory=$false, ParameterSetName='VM Only')]
+   [Parameter(Mandatory=$true, ParameterSetName='Auto Install')]
+   [switch]$AutoInstallASDK,
 
-        [Parameter(Mandatory=$false)]
-        [string]$AzureADTenant,
+   [Parameter(Mandatory=$false, ParameterSetName='Use Existing SA')]
+   [Parameter(Mandatory=$false, ParameterSetName='VM Only')]
+   [Parameter(Mandatory=$true, ParameterSetName='Auto Install')]
+   [string]$AzureADTenant,
 
-        [Parameter(Mandatory=$false)]
-        [pscredential]$AzureADGlobalAdminCredential
-    )
+   [Parameter(Mandatory=$false, ParameterSetName='Use Existing SA')]
+   [Parameter(Mandatory=$false, ParameterSetName='VM Only')]
+   [Parameter(Mandatory=$true, ParameterSetName='Auto Install')]
+   [pscredential]$AzureADGlobalAdminCredential,
 
+   [Parameter(Mandatory=$false, ParameterSetName='Use Existing SA')]
+   [Parameter(Mandatory=$false, ParameterSetName='VM Only')]
+   [Parameter(Mandatory=$false, ParameterSetName='Auto Install')]
+   [Bool]$UseAzCopy = $true #setting the value to 'false' will utilize Start-AzStorageBlobCopy
+)
+
+#region Functions
+
+function DownloadWithRetry([string] $Uri, [string] $DownloadLocation, [int] $Retries = 5, [int]$RetryInterval = 10)
+{
+    while($true)
+    {
+        try
+        {
+            [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+            Start-BitsTransfer -Source $Uri -Destination $DownloadLocation -DisplayName $Uri
+            break
+        }
+        catch
+        {
+            $exceptionMessage = $_.Exception.Message
+            Write-Host "Failed to download '$Uri': $exceptionMessage"
+            if ($retries -gt 0) {
+                $retries--
+                Write-Host "Waiting $RetryInterval seconds before retrying. Retries left: $Retries"
+                Clear-DnsClientCache
+                Start-Sleep -Seconds $RetryInterval
+ 
+            }
+            else
+            {
+                $exception = $_.Exception
+                throw $exception
+            }
+        }
+    }
+}
+
+#endregion
+    
 #Requires -Version 5
 #Requires -Module @{ ModuleName = 'Az.Accounts'; ModuleVersion = '2.2.1' }, @{ ModuleName = 'Az.Storage'; ModuleVersion = '3.0.0' }, @{ ModuleName = 'Az.Resources'; ModuleVersion = '3.0.1' }
 
 #region variables
 
+$templateUri = "https://raw.githubusercontent.com/yagmurs/AzureStack-VM-PoC/master/ARMv2/azuredeploy.json"
 $sleepTimer = 60
 $container = "asdk"
 $saPrefix = "asdk"
@@ -186,19 +272,45 @@ else
    
    New-AzStorageContainer -Name $container -Context $sa.context
    
-   Start-AzStorageBlobCopy -AbsoluteUri $sourceUri -DestContainer $container -DestContext $sa.context -DestBlob "$version.vhd" -ConcurrentTaskCount 100 -Force
-   
-   do {
-      Start-Sleep -Seconds $sleepTimer
-      $result = Get-AzStorageAccount -Name $sa.StorageAccountName -ResourceGroupName $ResourceGroupName | Get-AzStorageBlob -Container $container | Get-AzStorageBlobCopyState
-      $remaining = [Math]::Round(($result.TotalBytes - $result.BytesCopied) / 1gb,2)
-      Write-Verbose -Message "Waiting copy to finish remaining $remaining GB"
-      if ($remaining -lt 60)
+   if ($UseAzCopy)
+   {
+      if (-not ($PSCloudShellUtilityModuleInfo))
       {
-         $sleepTimer = 10
+         $azCopyTemp = [System.IO.Path]::GetTempPath()
+         Write-Verbose -Message "Downloading azCopy to $azCopyTemp"
+         $azcopyDestFilePath = Join-Path -Path $azCopyTemp -ChildPath "azcopy.zip"
+         $azcopyDestFolderPath = Join-Path -Path $azCopyTemp -ChildPath "azcopy"
+         DownloadWithRetry -Uri https://aka.ms/downloadazcopy-v10-windows -DownloadLocation $azcopyDestFilePath
+         Unblock-File -Path $azcopyDestFilePath
+         Expand-Archive -Path $azcopyDestFilePath -DestinationPath $azcopyDestFolderPath
+         $azCopyExePath = Get-ChildItem -Path $azcopyDestFolderPath -Recurse | Where-Object Name -eq azcopy.exe | Select-Object -ExpandProperty FullName
       }
-   } until ($result.Status -eq "success") 
+      else 
+      {
+         $azCopyExePath = "azcopy"
+      }
 
+      $sastoken = New-AzStorageContainerSASToken -Context $sa.Context -name $container -Permission racwdl
+      $destination = $sa.Context.BlobEndPoint+$container+$sastoken
+
+      & $azCopyExePath cp $sourceUri $destination
+   }
+   else
+   {
+      Start-AzStorageBlobCopy -AbsoluteUri $sourceUri -DestContainer $container -DestContext $sa.context -DestBlob "$version.vhd" -ConcurrentTaskCount 100 -Force
+   
+      do {
+         Start-Sleep -Seconds $sleepTimer
+         $result = Get-AzStorageAccount -Name $sa.StorageAccountName -ResourceGroupName $ResourceGroupName | Get-AzStorageBlob -Container $container | Get-AzStorageBlobCopyState
+         $remaining = [Math]::Round(($result.TotalBytes - $result.BytesCopied) / 1gb,2)
+         Write-Verbose -Message "Waiting copy to finish remaining $remaining GB"
+         if ($remaining -lt 60)
+         {
+            $sleepTimer = 10
+         }
+      } until ($result.Status -eq "success") 
+   }
+   
    $osDiskVhdUri = $sa.PrimaryEndpoints.Blob + "$container/$version.vhd"
 }
 
@@ -235,5 +347,5 @@ else
 }
 
 New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -Name AzureStackonAzureVM `
-   -TemplateUri "https://raw.githubusercontent.com/yagmurs/AzureStack-VM-PoC/master/ARMv2/azuredeploy.json" `
+   -TemplateUri $templateUri `
    -TemplateParameterObject $templateParameterObject
